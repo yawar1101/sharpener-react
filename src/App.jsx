d40fea90c5a81fc2d48f10d53be3779d2f99069a
yawar1101 <mohdyawars@gmail.com>
2024-02-11 14:06:35 +0530

    Intial Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import ExpenseItem from './components/ExpenseItem'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+        <div>
+            <h2>
+                Let's get started
+            </h2>
+            <ExpenseItem />
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ExpenseItem.jsx
@@ -0,0 +1,11 @@
+export default function ExpenseItem() {
+    return (
+        <>
+            <p>
+            Food Rs 10
+            Petrol Rs 100
+            Movies Rs 200
+            </p>
+        </>
+    )
+}
